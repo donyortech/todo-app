b03a4a50d428707bee7ac1f85098a33a8b32b008
donyortech <donyorgaoli@qq.com>
2022-10-18 09:20:41 +0800

    I deleted the unnecessary parts of  file

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h2>My Vue TodoApp</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped></style>
