96d066e909d8df200b320c4ac2c36001515edcd6
donyortech <donyorgaoli@qq.com>
2022-10-18 10:57:43 +0800

    I added method

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,14 +1,75 @@
 <template>
   <div class="container">
-    <h2>My Vue TodoApp</h2>
+    <h2 class="text-center mt-5">My Vue Todo App</h2>
+
+    <!-- Inputs -->
+    <div class="d-flex">
+      <input
+        v-model="task"
+        type="text"
+        placeholder="Enter task"
+        class="form-control"
+      />
+      <button @click="submitTask" class="btn btn-warning rounded-0">
+        SUBMIT
+      </button>
+    </div>
+
+    <!-- Task table -->
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Status</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>{{ task.name }}</td>
+          <td>{{ task.status }}</td>
+          <td>
+            <div class="text-center">
+              <span class="fa fa-pen"></span>
+            </div>
+          </td>
+          <td>
+            <div class="text-center">
+              <span class="fa fa-trash"></span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "",
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      task: "Hello World",
+      tasks: [
+        {
+          name: "Style banana from the store.",
+          status: "to-do",
+        },
+        {
+          name: "Eat 1kg chocolate in 1 hour",
+          status: "in-progress",
+        },
+      ],
+    };
+  },
+  methods: {
+    submitTask() {
+      console.log(this.task);
+    },
   },
 };
 </script>
