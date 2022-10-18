b3b982160230f27b550cd476536a3f3ab16fa740
donyortech <donyorgaoli@qq.com>
2022-10-18 17:22:42 +0800

    finish

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="container">
-    <h2 class="text-center mt-5">My Vue Todo App</h2>
+  <div class="container" style="max-width: 600px">
+    <!-- Heading -->
+    <h2 class="text-center mt-5">Todo App</h2>
 
-    <!-- Inputs -->
-    <div class="d-flex">
+    <!-- Input -->
+    <div class="d-flex mt-5">
       <input
-        v-model="task"
         type="text"
+        v-model="task"
         placeholder="Enter task"
-        class="form-control"
+        class="w-100 form-control"
       />
-      <button @click="submitTask" class="btn btn-warning rounded-0">
+      <button class="btn btn-warning rounded-0" @click="submitTask">
         SUBMIT
       </button>
     </div>
@@ -20,23 +21,39 @@
       <thead>
         <tr>
           <th scope="col">Task</th>
-          <th scope="col">Status</th>
+          <th scope="col" style="width: 120px">Status</th>
           <th scope="col" class="text-center">#</th>
           <th scope="col" class="text-center">#</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(task, index) in tasks" :key="index">
-          <td>{{ task.name }}</td>
-          <td>{{ task.status }}</td>
           <td>
-            <div class="text-center" @click="editTask(index)">
-              <span class="fa fa-pen"></span>
-            </div>
+            <span :class="{ 'line-through': task.status === 'finished' }">
+              {{ task.name }}
+            </span>
           </td>
           <td>
-            <div class="text-center" @click="deleteTask(index)">
-              <span class="fa fa-trash"></span>
+            <span
+              class="pointer noselect"
+              @click="changeStatus(index)"
+              :class="{
+                'text-danger': task.status === 'to-do',
+                'text-success': task.status === 'finished',
+                'text-warning': task.status === 'in-progress',
+              }"
+            >
+              {{ capitalizeFirstChar(task.status) }}
+            </span>
+          </td>
+          <td class="text-center">
+            <div @click="deleteTask(index)">
+              <span class="fa fa-trash pointer"></span>
+            </div>
+          </td>
+          <td class="text-center">
+            <div @click="editTask(index)">
+              <p class="fa fa-pen pointer"></p>
             </div>
           </td>
         </tr>
@@ -47,44 +64,105 @@
 
 <script>
 export default {
-  name: "",
+  name: "HelloWorld",
   props: {
     msg: String,
   },
+
   data() {
     return {
       task: "",
-      editTask: null,
+      editedTask: null,
+      statuses: ["to-do", "in-progress", "finished"],
+
+      /* Status could be: 'to-do' / 'in-progress' / 'finished' */
       tasks: [
         {
-          name: "Style banana from the store.",
+          name: "Steal bananas from the supermarket.",
           status: "to-do",
         },
         {
-          name: "Eat 1kg chocolate in 1 hour",
+          name: "Eat 1 kg chocolate in 1 hour.",
           status: "in-progress",
+        },
+        {
+          name: "Create YouTube video.",
+          status: "finished",
         },
       ],
     };
   },
+
   methods: {
-    submitTask() {
-      if (this.task.length === 0) return;
-      this.tasks.push({
-        name: this.task,
-        status: "to-do",
-      });
-      this.task = "";
+    /**
+     * Capitalize first character
+     */
+    capitalizeFirstChar(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
     },
+
+    /**
+     * Change status of task by index
+     */
+    changeStatus(index) {
+      let newIndex = this.statuses.indexOf(this.tasks[index].status);
+      if (++newIndex > 2) newIndex = 0;
+      this.tasks[index].status = this.statuses[newIndex];
+    },
+
+    /**
+     * Deletes task by index
+     */
     deleteTask(index) {
       this.tasks.splice(index, 1);
     },
+
+    /**
+     * Edit task
+     */
     editTask(index) {
       this.task = this.tasks[index].name;
-      this.editTask = index;
+      this.editedTask = index;
+    },
+
+    /**
+     * Add / Update task
+     */
+    submitTask() {
+      if (this.task.length === 0) return;
+
+      /* We need to update the task */
+      if (this.editedTask != null) {
+        this.tasks[this.editedTask].name = this.task;
+        this.editedTask = null;
+      } else {
+        /* We need to add new task */
+        this.tasks.push({
+          name: this.task,
+          status: "todo",
+        });
+      }
+
+      this.task = "";
     },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+.noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+}
+.line-through {
+  text-decoration: line-through;
+}
+</style>
